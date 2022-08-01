7f1df513f5e36a67f7218c004bf5d2d5c6b2f9c1
John Bellardo <bellardo@calpoly.edu>
2022-08-01 11:25:51 +0000

    Use the correct version of polyxdr

--- package/polysat/polyxdr/polyxdr.mk
+++ package/polysat/polyxdr/polyxdr.mk
@@ -4,11 +4,13 @@
 #
 #################################################################################
 
-POLYXDR_VERSION = 1.1
-POLYXDR_SOURCE = polyxdr-$(PYTHON_REGEX_VERSION).tar.gz
-POLYXDR_SITE = $(call github,PolySat,polyxdr,v$(POLYXDR_VERSION))
+POLYXDR_VERSION = v1.2
+POLYXDR_SITE:=https://github.com/PolySat/polyxdr.git
+POLYXDR_SITE_METHOD:=git
 POLYXDR_SETUP_TYPE = setuptools
 POLYXDR_LICENSE = GPL-2.0
 POLYXDR_LICENSE_FILES = LICENSE
-	
+POLYXDR_DEPENDENCIES=python-pyparsing
+HOST_POLYXDR_DEPENDENCIES=host-python-pyparsing host-python-tenjin
+
 $(eval $(host-python-package))
