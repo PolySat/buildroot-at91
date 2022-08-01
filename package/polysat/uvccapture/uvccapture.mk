67f35254f90d74d6ad11ae969d6600a716dd7b6a
John Bellardo <bellardo@calpoly.edu>
2022-08-01 16:19:19 +0000

    add uvccapture package

---
+++ package/polysat/uvccapture/uvccapture.mk
@@ -0,0 +1,34 @@
+#############################################################
+#
+# Polysat customized uvccapture
+#
+#############################################################
+UVCCAPTURE_SITE:=https://github.com/PolySat/uvccapture.git
+UVCCAPTURE_SITE_METHOD:=git
+UVCCAPTURE_INSTALL_TARGET=YES
+#UVCCAPTURE_DEPENDENCIES=jpeg polysat_fsw
+UVCCAPTURE_DEPENDENCIES=jpeg libgpio
+
+ifeq ($(BR2_PACKAGE_UVCCAPTURE_version_head),y)
+   UVCCAPTURE_VERSION:=$(shell git ls-remote $(UVCCAPTURE_SITE) | grep HEAD | head -1 | sed -e 's/[ \t]*HEAD//')
+endif
+
+ifeq ($(BR2_PACKAGE_UVCCAPTURE_version_custom),y)
+   UVCCAPTURE_VERSION=$(subst ",,$(BR2_PACKAGE_UVCCAPTURE_CONFIG_CUSTOM_VERSION_STR))
+endif
+
+ifeq ($(BR2_PACKAGE_UVCCAPTURE),y)
+   UVCCAPTURE_VERSION?=$(shell git ls-remote $(UVCCAPTURE_SITE) | grep HEAD | head -1 | sed -e 's/[ \t]*HEAD//')
+else
+   UVCCAPTURE_VERSION?=v0.00
+endif
+
+define UVCCAPTURE_BUILD_CMDS
+ $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define UVCCAPTURE_INSTALL_TARGET_CMDS
+   $(MAKE) -C $(@D) INC_PATH=$(STAGING_DIR)/usr/include LIB_PATH=$(TARGET_DIR)/usr/lib BIN_PATH=$(TARGET_DIR)/usr/bin SBIN_PATH=$(TARGET_DIR)/usr/sbin ETC_PATH=$(TARGET_DIR)/etc LOCAL_BIN_PATH=$(TARGET_DIR)/usr/local/bin LOCAL_SBIN_PATH=$(TARGET_DIR)/usr/local/sbin install
+endef
+
+$(eval $(generic-package))
